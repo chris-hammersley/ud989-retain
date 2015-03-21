f2f77894c90c30f6adfd86d7e15cd5ec527d9d24
chris-hammersley <chris.hammersley@gmail.com>
2015-03-20 22:14:08 -0700

    Added Date

--- js/retain.js
+++ js/retain.js
@@ -1,16 +1,19 @@
 $(function(){
 
     var model = {
+        // initial function that uses JSON to store note if there's no local storage available
         init: function() {
             if (!localStorage.notes) {
                 localStorage.notes = JSON.stringify([]);
             }
         },
+        // function to add note to local or JSON storage for later retrieval
         add: function(obj) {
             var data = JSON.parse(localStorage.notes);
             data.push(obj);
             localStorage.notes = JSON.stringify(data);
         },
+        // function to read all notes in local or JSON storage; should this be in Octopus?
         getAllNotes: function() {
             return JSON.parse(localStorage.notes);
         }
@@ -18,17 +21,19 @@ $(function(){
 
 
     var octopus = {
+        // function to take note from Model and add to View
         addNewNote: function(noteStr) {
             model.add({
-                content: noteStr
+                content: noteStr,
+                submitDate: Date.now()
             });
             view.render();
         },
-
+        // function to return filtered list of notes - in this case all notes
         getNotes: function() {
-            return model.getAllNotes();
+            return model.getAllNotes().reverse();
         },
-
+        // function to call both model & view initializations
         init: function() {
             model.init();
             view.init();
@@ -37,6 +42,7 @@ $(function(){
 
 
     var view = {
+        // function to display form field in View, assign variable to note entry and send to Octopus to be recorded
         init: function() {
             this.noteList = $('#notes');
             var newNoteForm = $('#new-note-form');
@@ -48,10 +54,11 @@ $(function(){
             });
             view.render();
         },
+        // function to display the notes in an unordered list in View
         render: function(){
             var htmlStr = '';
             octopus.getNotes().forEach(function(note){
-                htmlStr += '<li class="note">'+
+                htmlStr += '<li class="note">'+ '<span class="note-date">' + new Date(note.submitDate).toString() + '</span>' +
                         note.content +
                     '</li>';
             });
